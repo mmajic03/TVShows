ad822757fe636a57289f8d3f22e8ba3b739bfa87
mmajic03 <monikamajic03@gmail.com>
2025-08-07 15:38:24 +0200

    added comments describing Supabase database connection

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,10 @@
+//NextAuth je sustav za autentifikaciju u Next.js aplikacijama.
+//OAuth je sigurni standard za prijavu korisnika preko vanjskih servisa(npr.GitHub).
+//Umjesto da korisnik unosi email i lozinku, koristi se GitHub prijava kojom se dohvaćaju podaci korisnika.
+//https://next-auth.js.org/getting-started/example
 import NextAuth from "next-auth";
 import { authOptions } from "@/lib/auth";
 
+//Handler prati i obrađuje zahteve za prijavu i odjavu korisnika
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
